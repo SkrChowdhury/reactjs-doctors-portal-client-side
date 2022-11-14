b1f16ac9f6a10d4546f95cce526fcd85ff7ab9ff
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-11-14 22:55:25 +0600

    testimonial and footer section added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -3,6 +3,7 @@ import Banner from '../Banner/Banner';
 import InfoCards from '../InfoCards/InfoCards';
 import MakeAppointment from '../MakeAppointment/MakeAppointment';
 import Services from '../Services/Services';
+import Testimonials from '../Testimonials/Testimonials';
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <InfoCards></InfoCards>
       <Services></Services>
       <MakeAppointment></MakeAppointment>
+      <Testimonials></Testimonials>
     </div>
   );
 };
