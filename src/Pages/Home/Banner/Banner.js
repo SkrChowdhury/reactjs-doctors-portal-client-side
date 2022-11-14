7b8ce15c33182fd9d896f2f4fb1c04039b50eb6d
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-11-14 21:08:03 +0600

    appointment section added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import chair from '../../../assets/images/chair.png';
+import PrimaryButton from '../../../components/PrimaryButton/PrimaryButton';
 
 const Banner = () => {
   return (
     <div className="hero">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <img src={chair} className=" rounded-lg lg:w-1/2 shadow-2xl" />
+        <img
+          src={chair}
+          className=" rounded-lg lg:w-1/2 shadow-2xl"
+          alt="chair"
+        />
         <div>
           <h1 className="text-5xl font-bold">Box Office News!</h1>
           <p className="py-6">
@@ -13,9 +18,7 @@ const Banner = () => {
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
           </p>
-          <button className="btn bg-gradient-to-r from-primary to-secondary text-white border-0">
-            Get Started
-          </button>
+          <PrimaryButton>Get Started</PrimaryButton>
         </div>
       </div>
     </div>
