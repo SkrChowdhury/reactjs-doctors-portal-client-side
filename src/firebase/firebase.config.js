e55a96c18514cb5e29668783e959120cebcbed9c
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-11-17 00:17:10 +0600

    login page and firebase added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyA58LdygqkLdyQ5EubJdumnx7JR7_lAJ10',
+  authDomain: 'reactjs-doctors-portal.firebaseapp.com',
+  projectId: 'reactjs-doctors-portal',
+  storageBucket: 'reactjs-doctors-portal.appspot.com',
+  messagingSenderId: '721261595715',
+  appId: '1:721261595715:web:01b3c74d734b5a4653d8f6',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
