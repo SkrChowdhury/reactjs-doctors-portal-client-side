98bbf2a14f9575bfda1f2fa36d1cc6e8310589bd
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-11-19 11:37:13 +0600

    react query added

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -1,3 +1,4 @@
+import { useQuery } from '@tanstack/react-query';
 import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
 import BookingModal from '../BookingModal/BookingModal';
@@ -7,18 +8,27 @@ const AvailableAppointments = ({ selectedDate }) => {
   const [appointmentOptions, setAppointmentOptions] = useState([]);
   const [treatment, setTreatment] = useState(null);
 
-  useEffect(() => {
-    fetch('http://localhost:5000/appointmentOptions')
-      .then((res) => res.json())
-      .then((data) => setAppointmentOptions(data));
-  }, []);
+  const { data: appointmentOptionsData = [] } = useQuery({
+    queryKey: ['appointmentOptions'],
+    queryFn: async () => {
+      const res = await fetch('http://localhost:5000/appointmentOptions');
+      const data = await res.json();
+      return data;
+    },
+  });
+
+  // useEffect(() => {
+  //   fetch('http://localhost:5000/appointmentOptions')
+  //     .then((res) => res.json())
+  //     .then((data) => setAppointmentOptions(data));
+  // }, []);
   return (
     <section className="my-16">
       <p className="text-center text-secondary  font-bold">
         Available Appointments on {format(selectedDate, 'PP')}
       </p>
       <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6">
-        {appointmentOptions.map((option) => (
+        {appointmentOptionsData.map((option) => (
           <AppointmentOption
             key={option._id}
             appointmentOption={option}
