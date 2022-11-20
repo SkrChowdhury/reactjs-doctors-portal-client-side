f059a5624971fb2fa5375363bdeea7c72511e189
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-11-21 00:58:47 +0600

    Create Make admin API limit the feature based on admin level

--- src/Pages/DashBoard/AllUsers/AllUsers.js
+++ src/Pages/DashBoard/AllUsers/AllUsers.js
@@ -1,8 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import toast from 'react-hot-toast';
 
 const AllUsers = () => {
-  const { data: users = [] } = useQuery({
+  const { data: users = [], refetch } = useQuery({
     queryKey: ['users'],
     queryFn: async () => {
       const res = await fetch('http://localhost:5000/users');
@@ -10,6 +11,22 @@ const AllUsers = () => {
       return data;
     },
   });
+
+  const handleMakeAdmin = (id) => {
+    fetch(`http://localhost:5000/users/admin/${id}`, {
+      method: 'PUT',
+      headers: {
+        authorization: `bearer ${localStorage.getItem('accessToken')}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          toast.success('Made Admin Successfully.');
+          refetch();
+        }
+      });
+  };
   return (
     <div>
       <h2 className="text-3xl">All Users</h2>
@@ -21,8 +38,8 @@ const AllUsers = () => {
               <th></th>
               <th>Name</th>
               <th>Email</th>
-              <th>Favorite Color</th>
-              <th>Favorite Color</th>
+              <th>Admin</th>
+              <th>Delete</th>
             </tr>
           </thead>
           <tbody>
@@ -32,8 +49,21 @@ const AllUsers = () => {
                 <th>{index + 1}</th>
                 <td>{user.name}</td>
                 <td>{user.email}</td>
-                <td>Blue</td>
-                <td>Blue</td>
+                <td>
+                  {user?.role !== 'admin' && (
+                    <button
+                      onClick={() => handleMakeAdmin(user._id)}
+                      className="btn btn-xs btn-primary text-white  "
+                    >
+                      Make Admin
+                    </button>
+                  )}
+                </td>
+                <td>
+                  <button className="btn btn-xs bg-red-500 hover:bg-red-600  text-white btn-outline">
+                    Delete
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
